9694161ae5a2bbc5739857927bf6a003a6afd2fc
JannisKohle <Jannis.Kohle@icloud.com>
2020-10-18 10:48:04 +0200

    completed dir set/get code

--- cli/kanban.py
+++ cli/kanban.py
@@ -10,14 +10,27 @@ def getConfig():
         with open(os.path.expanduser("~/.config/kanban.json"), "r+") as f:
             return json.load(f)
     else: # config file doesn't exist
-        return 1
+        return "error"
 
 ##########
 
 if argv[1] == "dir":
     if argv[2] == "set":
         if len(argv) == 4:
-            pass
+            path = argv[3]
+            if "kanban.json" in os.path.expanduser("~/.config"): # config file already exists -> overwrite it
+                with open(os.path.expanduser("~/.config/kanban.json"), "r+") as f:
+                    content = json.load(f)
+                    content["path"] = path
+                    f.truncate(0)
+                    json.dump(content, f)
+            else: # config file doesn't exist
+                os.system(f"touch {os.path.expanduser('~/.config/kanban.json')}")
+                with open(os.path.expanduser("~/.config/kanban.json"), "r+") as f:
+                    json.dump({"path": path, "opened": None}, f)
+
+            print("Successfully set path to directory for storing boards.")
+            exit()
 
         else:
             print("Invalid command")
@@ -25,7 +38,8 @@ if argv[1] == "dir":
 
     elif ergv[2] == "get":
         if len(argv) == 3:
-            pass
+            print(getConfig()["path"]
+            exit()
 
         else:
             print("Invalid command")
